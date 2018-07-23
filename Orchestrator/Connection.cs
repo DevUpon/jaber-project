d9364e133156ed857174aefba2ecb17ee3e34a2e
Matthieu <matthieubalondrade@outlook.fr>
2018-07-23 16:57:42 +0200

    Initialisation orchestrator for POC

---
+++ Orchestrator/Connection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Orchestrator
+{
+    class Connection : Socket
+    {
+        public Connection(SocketInformation socketInformation) : base(socketInformation)
+        {
+        }
+
+        public Connection(SocketType socketType, ProtocolType protocolType) : base(socketType, protocolType)
+        {
+        }
+
+        public Connection(AddressFamily addressFamily, SocketType socketType, ProtocolType protocolType) : base(addressFamily, socketType, protocolType)
+        {
+        }
+    }
+}
