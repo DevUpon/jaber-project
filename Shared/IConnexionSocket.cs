d50d3858de281cd27e265db48e7acc849fea8175
Matthieu <matthieubalondrade@outlook.fr>
2018-09-05 20:00:52 +0200

    fixing copy paste from nico

---
+++ Shared/IConnexionSocket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+public interface IConnexionSocket
+{
+
+    String getAdresse();
+    String getPort();
+    Boolean ouvrirConnexion();
+    Boolean fermerConnexion();
+    Boolean verifierConnexion();
+    Socket getSocket();
+    String recevoir();
+    Boolean envoyer(String data);
+}
