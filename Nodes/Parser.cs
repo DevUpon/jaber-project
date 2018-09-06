b57fe336ce6ccff40aed3955d0d8b1ccf624c66f
Grabie Eliott <eliott.grabie@gmail.com>
2018-09-06 19:08:36 +0200

    IHM + Parser

---
+++ Nodes/Parser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nodes
+{
+    class Parser
+    {
+        public Parser()
+        {
+        }
+        public List Parse(String sFragDNA)
+        {
+            Dictionary Dict
+            List lDictResult = 
+            return lDictResult;
+        }
+    }
+}
