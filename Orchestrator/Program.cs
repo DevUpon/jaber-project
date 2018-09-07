9a040378e69d02f0ea7436a6d33f893fc9d69c44
DESKTOP-U4K4SH7\Faurever <jeromefaure93@orange.fr>
2018-09-07 13:26:12 +0200

    Added interface

--- Orchestrator/Program.cs
+++ Orchestrator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Orchestrator
 {
@@ -8,13 +9,22 @@ namespace Orchestrator
         static void Main(string[] args)
         {
             Maestro orch = new Maestro(1);
-            List<List<String>> dnaList= orch.CreateDnaFragChunk(@"C:\Users\Faurever\Desktop\genomeTest.txt", 2);
+            List<List<String>> dnaList= orch.CreateDnaFragChunk(@"C:\temp\DNAFile.txt", 2);
             orch.Connecter(2);
             orch.WaitCompletedConnexion();
             orch.Envoyer(dnaList);
             string result = orch.Recevoir();
             Dictionary<string, int> reducing = orch.ReduceString(result);
             Console.WriteLine(reducing);
+
+            string path = @"C:\temp\ResultFile.txt";
+            using (StreamWriter outputFile = new StreamWriter(path))
+            {
+                foreach(var line in reducing)
+                {
+                    outputFile.WriteLine("[{0} : {1}]", line.Key, line.Value);
+                }
+            }
         }
     }
 }
