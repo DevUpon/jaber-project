b7b660481ada8cd69c6bf0b346e76d44e16f3469
DESKTOP-U4K4SH7\Faurever <jeromefaure93@orange.fr>
2018-09-07 10:08:42 +0200

    Fix conflict forgotten

--- Nodes/Node.cs
+++ Nodes/Node.cs
@@ -1,26 +1,17 @@
 ﻿using System;
-<<<<<<< HEAD
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-=======
 using Shared;
-using System.Collections.Generic;
->>>>>>> 8450a98b6824e525db4a95bfd156b8b6d1e5ff33
 
-<<<<<<< HEAD
+
+
 /// </Bouchon>
-=======
->>>>>>> 20aab16b997015dcb7d3efbd366aa089cf3519f1
+
 namespace Nodes
 {
     public class Node
     {
         private List<String> dnaFragment;
         private IConnexionNode connexion;
-        private IReduce reduce;
 
         public Node()
         {
--- Orchestrator/Program.cs
+++ Orchestrator/Program.cs
@@ -1,27 +1,15 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Nodes;
 
 namespace Orchestrator
-=======
-﻿namespace Orchestrator
->>>>>>> 1c38a83d193af3e316d3e6b698f38dfea4f44db3
 {
     class Program
     {
         static void Main(string[] args)
         {
             Maestro orch = new Maestro(1);
-<<<<<<< HEAD
             List<List<String>> dnaList= orch.CreateDnaFragChunk(@"C:\Users\Faurever\Desktop\genomeTest.txt", 2);
             orch.Connecter(2);
-=======
-            orch.Connecter();
->>>>>>> 1c38a83d193af3e316d3e6b698f38dfea4f44db3
             orch.WaitCompletedConnexion();
             orch.Envoyer(dnaList);
             orch.Recevoir();
